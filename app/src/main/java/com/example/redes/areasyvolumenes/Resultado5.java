7cc4867b609b0125411a0d7de8fbd4d81aab2030
Andres Cantillo <acantill26@gmail.com>
2017-09-27 15:21:31 -0500

    redes

---
+++ app/src/main/java/com/example/redes/areasyvolumenes/Resultado5.java
@@ -0,0 +1,17 @@
+package com.example.redes.areasyvolumenes;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Resultado5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado5);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
